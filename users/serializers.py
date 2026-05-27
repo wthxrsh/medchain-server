b62e9e366953e088ef264ab7f963835e6de8d6fc
Dev Agent <dev@medchain.com>
2026-05-27 09:22:52 +0530

    refactor: update user serializer to handle roles and secure fields

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,15 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'email', 'role', 'password')
+        fields = ('id', 'email', 'first_name', 'last_name', 'role', 'password')
         extra_kwargs = {'password': {'write_only': True}}
         
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
             password=validated_data['password'],
-            role=validated_data.get('role', CustomUser.RoleChoices.PATIENT)
+            role=validated_data.get('role', CustomUser.RoleChoices.PATIENT),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
         )
         return user
